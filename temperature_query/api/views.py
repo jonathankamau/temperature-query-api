ac08a634e26df2d33357b025cfd1ebf1b5b40d3c
jonathankamau <kamaujonathan@gmail.com>
2021-02-05 12:45:13 +0300

    [Feature]: modify view to use the APIView library and include error handling

--- temperature_query/api/views.py
+++ temperature_query/api/views.py
@@ -2,23 +2,69 @@
 import os
 import json
 import urllib
-from rest_framework import viewsets, mixins
+from rest_framework import status
+from rest_framework.views import APIView
 from rest_framework.response import Response
 
 
-class TemperatureQuery(viewsets.GenericViewSet, mixins.ListModelMixin):
+class TemperatureQuery(APIView):
     """Endpoint to query for temperature details."""
 
-    def get_temperature(self, city, number_of_days):
-        """Retrieve temperature data from weather API."""
+    def get(self, request, city, number_of_days):
+        """
+        Retrieve temperature data from weather API.
+
+        return the min, max, average and median temperature data
+        for that period.
+        """
+        if not city.isalpha():
+            return Response(
+                {
+                    "Error":
+                    f"{city} is invalid! Please provide a valid city name"
+                },
+                status=status.HTTP_400_BAD_REQUEST)
+
+        if not number_of_days.isnumeric():
+            return Response(
+                {
+                    'Error':
+                    f"{number_of_days} is invalid! "
+                    "Please provide a valid digit"
+                },
+                status=status.HTTP_400_BAD_REQUEST)
+
         params = urllib.parse.urlencode(
-            {"key": os.getenv("WEATHER_API_KEY"), "q": city,
-             "days": number_of_days}
+            {
+                "key": os.getenv("WEATHER_API_KEY"),
+                "q": city,
+                "days": number_of_days
+            }
         )
 
         url = f"{os.getenv('WEATHER_API_URL')}?{params}"
 
-        temperature_details = json.load(urllib.request.urlopen(url))
+        temperature_details = []
+        try:
+            temperature_details = json.load(urllib.request.urlopen(url))
+        except urllib.error.HTTPError as e:
+            if e.getcode() == 400:
+                return Response(
+                    {
+                        "Error":
+                        f"Could not get temperature details for {city} "
+                        "Please provide a valid city name"
+                    },
+                    status=status.HTTP_400_BAD_REQUEST)
+
+        if len(temperature_details) == 0:
+
+            return Response(
+                {
+                    "Error":
+                    f"Could not retrieve temperature data for {city}"
+                },
+                status=status.HTTP_404_NOT_FOUND)
 
         min_list = [
             temp["day"]["mintemp_f"]
@@ -41,5 +87,6 @@ class TemperatureQuery(viewsets.GenericViewSet, mixins.ListModelMixin):
                 "minimum": min(min_list),
                 "average": sum(avg_list) / len(avg_list),
                 "median": (median_list[mid] + median_list[~mid]) / 2,
-            }
+            },
+            status=status.HTTP_200_OK
         )
