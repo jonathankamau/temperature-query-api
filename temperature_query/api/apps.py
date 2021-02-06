92e2c0df0e695486f49228fa9ff7f6cc21210c4b
jonathankamau <kamaujonathan@gmail.com>
2021-02-06 17:50:23 +0300

    [Feature]: modify root folder name to use the shorter and more appropriate 'src'

--- temperature_query/api/apps.py
+++
@@ -1,9 +0,0 @@
-"""API Configurations."""
-
-from django.apps import AppConfig
-
-
-class ApiConfig(AppConfig):
-    """Required configurations for the API."""
-
-    name = "temperature_query.api"
--- temperature_query/api/utils/__init__.py
+++
@@ -1,3 +0,0 @@
-"""Utility imports."""
-from temperature_query.api.utils.serializers import ResultSerializer
-from temperature_query.api.utils.compute_temperature import ComputeTemperature
