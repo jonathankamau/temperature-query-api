ed970fe2e3ac77ae17b04f589f4534cda0f242aa
jonathankamau <kamaujonathan@gmail.com>
2021-02-07 18:24:52 +0300

    [Chore]: add the method that gets the response from the public weatherapi

--- src/api/utils/__init__.py
+++ src/api/utils/__init__.py
@@ -2,3 +2,4 @@
 from src.api.utils.serializers import ResultSerializer
 from src.api.utils.compute_temperature import ComputeTemperature
 from src.api.utils.errors import ErrorResponses
+from src.api.utils.weatherapi_response import grab_weather_api_response
