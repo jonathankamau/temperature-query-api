b8270e083699822f8ac7a2b1adf6849279b002e1
jonathankamau <kamaujonathan@gmail.com>
2021-02-07 17:20:59 +0300

    [Chore]: add the error response class

--- src/api/utils/__init__.py
+++ src/api/utils/__init__.py
@@ -1,3 +1,4 @@
 """Utility imports."""
 from src.api.utils.serializers import ResultSerializer
 from src.api.utils.compute_temperature import ComputeTemperature
+from src.api.utils.errors import ErrorResponses
